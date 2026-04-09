98d4d7cb9ff0d20c26c0ae89cbbf9d54ff9541df
kaitlinchoffmann <kaitlinchoffmann@gmail.com>
2026-04-09 13:40:11 -0400

    Add eventListener to login form. Create login function to check password, and to create print and user object

--- public/scripts/user.js
+++ public/scripts/user.js
@@ -1,84 +1,29 @@
-/*
-+------------------------------------+
-|             User                   |
-+------------------------------------+
-| - userId                           |
-| + username                         |
-| - password                         |
-+------------------------------------+
-| + User(userId, username, password) |
-| + getUserId(): userId              |
-| + setPassword(password): void      |
-+------------------------------------+
-*/
 
-class User {
-  username
-  #userId
-  #password
+// 1. grab the form and assign to a variable
+let loginForm = document.getElementById("loginForm")
 
-  constructor(username, userId, password) {
-    this.username = username;
-    this.#password = password;
-    this.#userId = userId;
-  }
+//2. add event listener to our form by using loginForm name
+// make sure to check to see if loginForm exists before adding event listener
+if(loginForm) loginForm.addEventListener('submit', login)
 
-  getUserId() {
-    return this.#userId;
-  }
+// 3. create function that will take in data from form and create a new User object
+function login(e) {
+    e.preventDefault()
 
-  setPassword(password) {
-    this.#password = password;
-  }
+    let username = document.getElementById("username").value
+    let password = document.getElementById("passwd").value
+    if(checkPassword(password)) {
+        const user = {
+            username: username,
+            password: password
+        }
 
-  getPassword() {
-    return this.#password;
-  }
+        console.log(user)
+    } else {
+        console.log("Password sucks! Do better.")
+    }
 }
 
-const u1 = new User("carl", 10, "badpassword")
-console.log(u1)
-
-console.log(u1.username)
-console.log(u1.getUserId())
-
-console.log(u1.getPassword())
-u1.setPassword("betterpassword")
-console.log(u1.getPassword())
-
-
-const users = [
-  {
-    userId: 3,
-    username: "cathy123",
-    following: true
-  },
-  {
-    userId: 2,
-    username: "spongebob12",
-    following: false
-  },
-  {
-    userId: 1,
-    username: "cathyiscool",
-    following: true
-  },
-  {
-    userId: 5,
-    username: "fredfredburger",
-    following: false
-  },
-  {
-    userId: 4,
-    username: "mindy1",
-    following: true
-  }
-];
-
-const followers = users.filter(i => i.following === true)
-const followers2 = users.map(i => i.userId)
-console.log(followers)
-console.log(followers2)
-
-const followers3 = users.filter(i => i.following === true).map(i => i.userId)
-console.log(followers3)
+function checkPassword(password) {
+    return true;
+}
