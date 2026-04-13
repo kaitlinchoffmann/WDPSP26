9b66495843a1989b9e408df21db0c1fec367c1fc
kaitlinchoffmann <kaitlinchoffmann@gmail.com>
2026-04-13 13:35:35 -0400

    Example with fetch

---
+++ public/scripts/fetchExample.js
@@ -0,0 +1,10 @@
+let muscleGroup = "biceps" // pretend we are retrieving this from a form
+let apiKey = "fgsgfu4223DDXG"
+
+fetch(`https://api.api-ninjas.com/v1/${apiKey}/exercises?muscle=${muscleGroup}`)
+    .then(response => response.json())
+    .then(data => {
+        // data.forEach(i => console.log(i.image))
+        console.log(data)
+    })
+    .catch(error => console.log(error))
