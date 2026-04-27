affeaf013b36e35a1eab6bb6a62ac9b076275f7e
kaitlinchoffmann <kaitlinchoffmann@gmail.com>
2026-04-27 14:10:27 -0400

    Update index.js to include CORs

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express()
 app.use(express.json())
 
 const userRoutes = require("./server/routes/user")
+
+//CORS middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");  
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");  
+  next();
+});
+
 app.use("/user", userRoutes)
 
 // instead of having a domain name like, www.bestrecipes.com, 
--- public/scripts/user.js
+++ public/scripts/user.js
@@ -28,4 +28,19 @@ function checkPassword(password) {
     return true;
 }
 
-export let user = "hi"
+// fetchData function: use for POST, PUT, and DELETE. 
+// Fetch method implementation:
+export async function fetchData(route = '', data = {}, methodType) {
+  const response = await fetch(`http://localhost:3000${route}`, {
+    method: methodType, // *POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  if (response.ok) {
+    return await response.json(); // parses JSON response into native JavaScript objects
+  } else {
+    throw await response.json();
+  }
+}
